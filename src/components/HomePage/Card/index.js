215fac771cabb79d46690fc7b1cd70a22b52fd7c
jon alden <jondevalden@gmail.com>
2019-10-22 13:36:58 -0500

    ingredients now displayed in an ordered list

--- src/components/HomePage/Card/index.js
+++ src/components/HomePage/Card/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import ingredients from "../../../images/ingredients.jpg";
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
@@ -12,10 +11,7 @@ import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
     card: {
@@ -50,56 +46,59 @@ export default function RecipeCard(props) {
     };
 
     return (
-            <Card className={classes.card}>
-                <CardHeader
-                    avatar={
-                        <Avatar aria-label="recipe" className={classes.avatar}>
-                            R
+        <Card className={classes.card}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                        R
                         </Avatar>
-                    }
-                    action={
-                        <IconButton aria-label="settings">
-                            <MoreVertIcon />
-                        </IconButton>
-                    }
-                    title={props.recipe.title}
-                />
-                <CardMedia
-                    className={classes.media}
-                    image={props.recipe.imgUrl}
-                    title={props.recipe.imgAlt}
-                />
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.recipe.description}
-                    </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
+                }
+                title={props.recipe.title}
+            />
+            <CardMedia
+                className={classes.media}
+                image={props.recipe.imgUrl}
+                title={props.recipe.imgAlt}
+            />
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {props.recipe.description}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                {/* <IconButton aria-label="add to favorites">
                         <FavoriteIcon />
                     </IconButton>
                     <IconButton aria-label="share">
                         <ShareIcon />
-                    </IconButton>
-                    <IconButton
-                        className={clsx(classes.expand, {
-                            [classes.expandOpen]: expanded,
-                        })}
-                        onClick={handleExpandClick}
-                        aria-expanded={expanded}
-                        aria-label="show more"
-                    >
-                        <ExpandMoreIcon />
-                    </IconButton>
-                </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                        <Typography paragraph>Method:</Typography>
-                        <Typography paragraph>
-                            {props.recipe.method}
-                        </Typography>
-                    </CardContent>
-                </Collapse>
-            </Card>
+                    </IconButton> */}
+                <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography paragraph>Ingredients:</Typography>
+                    <Typography paragraph>
+                        <ol>
+                            {props.recipe.ingredients.map(ingredient => {
+                                return <li>{ingredient}</li>
+                            })}
+                        </ol>
+                    </Typography>
+                    <Typography paragraph>Method:</Typography>
+                    <Typography paragraph>
+                        {props.recipe.method}
+                    </Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
     );
 }
